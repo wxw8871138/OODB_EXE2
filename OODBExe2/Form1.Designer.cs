d2b54051c6ea76c1fdbc5e4de80853804cb8cdfd
WangXinwei <WangXinwei@DESKTOP-6VHU3KN>
2016-09-29 15:50:33 +0800

    Add project files.

---
+++ OODBExe2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace OODBExe2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 139);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(562, 285);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(394, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 452);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 5, 7, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OODBExe2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OODBExe2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            //1
+
+            //var q = from x in context.Movies select x;
+            //dataGridView1.DataSource = q.ToList();
+
+            dataGridView1.DataSource = context.Movies.ToList();
+
+            //2
+
+            //var q = from x in context.Movies orderby x.MovieTitle select x;
+            //dataGridView1.DataSource = q.ToList();
+
+            //dataGridView1.DataSource = context.Movies.OrderBy(x => x.MovieTitle).ToList();
+
+            //3
+
+            //var q = from x in context.Movies orderby x.RentalPrice descending select x;
+            //dataGridView1.DataSource = q.ToList();
+
+            //dataGridView1.DataSource = context.Movies.OrderByDescending(x => x.RentalPrice).ToList();
+
+            //4
+
+            //var q = from x in context.Movies where x.Rating =="R" select x;
+            //dataGridView1.DataSource = q.ToList();
+
+            //dataGridView1.DataSource = context.Movies.Where(x => x.Rating == "R").ToList();
+
+            //5
+
+            //var q = from x in context.Movies where x.MovieType == "Sci-fi" && x.Producer == "Warner" select x;
+            //dataGridView1.DataSource = q.ToList();
+
+            //dataGridView1.DataSource = context.Movies.Where(x => x.MovieType == "Sci-fi" && x.Producer == "Warner").ToList();
+
+            //6
+
+            //label1.Text = context.Movies.Count(x => x.MovieType == "Action").ToString();
+
+            //7
+
+            //label1.Text = context.Movies.Where(x => x.MovieType == "Action").Sum(x => x.TotalStock).ToString();
+
+            //8
+
+            //label1.Text = context.Movies.Where(x => x.MovieType == "Comedy").Average(x => x.RentalPrice).ToString();
+
+            //9
+
+            //var q = from x in context.Movies select new { x.VideoCode, x.MovieTitle, x.RentalPrice };
+            //dataGridView1.DataSource = q.ToList();
+
+            //dataGridView1.DataSource= context.Movies.Select(x => new { x.MovieTitle, x.VideoCode, x.Rating }).ToList();
+
+            //10
+
+            //var q = from x in context.Movies
+            //        group x by x.MovieType into g
+            //        select new { MovieType = g.Key, copies = g.Sum(y => y.TotalStock) };
+            //dataGridView1.DataSource = q.ToList();
+
+            //？？dataGridView1.DataSource = context.Movies.GroupBy(x=>x.MovieType).Select()
+
+        }
+    }
+}
